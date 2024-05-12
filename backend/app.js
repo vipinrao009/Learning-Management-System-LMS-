7509544b6e29f9e12ad3d6e110e8fcf0f6b15271
vipinrao009 <vipinrao2018@gmail.com>
2024-05-12 22:18:58 +0530

    cors

--- backend/app.js
+++ backend/app.js
@@ -20,7 +20,7 @@ app.use(cookieParser());
 app.use(express.urlencoded({extended:true}));
 app.use(
   cors({
-    origin: [process.env.FRONTEND_URL],
+    origin: [process.env.FRONTEND_URL,"http://localhost:5173"],
     credentials: true
   })
 );
