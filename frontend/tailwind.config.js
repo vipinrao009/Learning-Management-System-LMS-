99f13a3405a47250b19b4066597a9a56501a10f6
vipinrao009 <vipinrao2018@gmail.com>
2025-01-28 04:22:03 +0530

    Update Tailwind CSS configuration to include line-clamp plugin and adjust content paths

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,12 +1,23 @@
 /** @type {import('tailwindcss').Config} */
+import daisyui from "daisyui";
+import lineClamp from "@tailwindcss/line-clamp";
+
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./src/**/*.{html,js,jsx,ts,tsx}"],
   theme: {
     extend: {},
   },
-  plugins: [require("daisyui",require("@tailwindcss/line-clamp"))],
-}
+  plugins: [daisyui, lineClamp],
+};
+
+// export default {
+//   content: [
+//     "./index.html",
+//     "./src/**/*.{js,ts,jsx,tsx}",
+//   ],
+//   theme: {
+//     extend: {},
+//   },
+//   plugins: [require("daisyui",require("@tailwindcss/line-clamp"))],
+// }
 
