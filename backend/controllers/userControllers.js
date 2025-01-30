44a65f3024d1725a16720cb433eccadf0127f742
vipinrao009 <vipinrao2018@gmail.com>
2025-01-31 02:21:45 +0530

    test 6

--- backend/controllers/userControllers.js
+++ backend/controllers/userControllers.js
@@ -83,7 +83,7 @@ const register = async (req, res, next) => {
 
   const CookieOptions = {
     maxAge: 7 * 24 * 60 * 60 * 1000, //7days
-    httpOnly:true,
+    httpOnly:true, 
     secure:true,
   };
 
